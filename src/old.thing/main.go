971dcdb98ba6d9a2b6c58d78c4f629833a5a9c7f
Pham.Van.Huy <phamvanhuy14@gmail.com>
2018-10-04 10:45:09 +0700

    int frontend

--- src/old.thing/main.go
+++ src/old.thing/main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/urfave/negroni"
+	"net/http"
 	"old.thing/routers"
 )
 
 func main() {
 	router := routers.InitRoutes()
-
-	router.HandleFunc("/home", func(w http.ResponseWriter, req *http.Request) {
-		// The "/" pattern matches everything, so we need to check
-		// that we're at the root here.
-		fmt.Fprintf(w, "Welcome to the home page!")
-
-	})
-
 	n := negroni.Classic()
 	n.UseHandler(router)
 
