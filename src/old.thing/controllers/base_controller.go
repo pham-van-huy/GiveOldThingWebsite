27238fa678996c1be1efd8e240ac4cc99e19cdb8
Phan Ngoc <phann123@Phans-MacBook-Air.local>
2018-10-06 11:33:22 +0700

    complete rest for post

--- src/old.thing/controllers/base_controller.go
+++ src/old.thing/controllers/base_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"reflect"
 )
 
 type payload interface {
@@ -35,4 +36,26 @@ func ResErrors(w http.ResponseWriter, data payload) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
 	w.Write(result)
+}
+
+func initializeStruct(t reflect.Type, v reflect.Value) {
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		ft := t.Field(i)
+		switch ft.Type.Kind() {
+		case reflect.Map:
+			f.Set(reflect.MakeMap(ft.Type))
+		case reflect.Slice:
+			f.Set(reflect.MakeSlice(ft.Type, 0, 0))
+		case reflect.Chan:
+			f.Set(reflect.MakeChan(ft.Type, 0))
+		case reflect.Struct:
+			initializeStruct(ft.Type, f)
+		case reflect.Ptr:
+			fv := reflect.New(ft.Type.Elem())
+			initializeStruct(ft.Type.Elem(), fv.Elem())
+			f.Set(fv)
+		default:
+		}
+	}
 }
